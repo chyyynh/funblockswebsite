4bfbfb4ae2c9fb093e84dee8f6c24233d0a40ecf
chiny <chinyuhsu1023@gmail.com>
2025-01-30 01:15:27 +0800

    edit favico & remove next/head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,13 @@ import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Noto_Sans_TC } from "next/font/google";
+
+const notoSansTC = Noto_Sans_TC({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -15,32 +22,33 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    metadataBase: new URL("https://funblocks.website"), // 替換為你的實際域名
+export const metadata: Metadata = {
+  metadataBase: new URL("https://funblocks.website"), // 替換為你的實際域名
+  title: "Funblocks",
+  description: "focus on the fully on chain game & autonomous world",
+  icons: {
+    icon: "/icon.png",
+  },
+  openGraph: {
     title: "Funblocks",
     description: "focus on the fully on chain game & autonomous world",
-    openGraph: {
-      title: "Funblocks",
-      description: "focus on the fully on chain game & autonomous world",
-      url: "https://funblocks.website",
-      images: [
-        {
-          url: "/images/funblocksvisual.jpg",
-          width: 1200,
-          height: 630,
-          alt: "Funblocks Visual",
-        },
-      ],
-    },
-    twitter: {
-      card: "summary_large_image",
-      title: "Funblocks",
-      description: "focus on the fully on chain game & autonomous world",
-      images: ["/images/funblocksvisual.jpg"],
-    },
-  };
-}
+    url: "https://funblocks.website",
+    images: [
+      {
+        url: "/images/funblocksvisual.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Funblocks Visual",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Funblocks",
+    description: "focus on the fully on chain game & autonomous world",
+    images: ["/images/funblocksvisual.jpg"],
+  },
+};
 
 export default function RootLayout({
   children,
@@ -50,7 +58,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${notoSansTC.className} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
         <Analytics />
